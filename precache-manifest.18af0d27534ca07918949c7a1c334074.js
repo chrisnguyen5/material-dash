767fdd9cba07e8a291c7e8753c2109a8eede504b
Chris Nguyen <chrisnguyen@Chriss-MacBook-Pro.local>
2020-08-12 12:56:01 -0700

    Updates

---
+++ precache-manifest.18af0d27534ca07918949c7a1c334074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb55f95e39381049f33e1f6896fd99d",
+    "url": "/material-dash/index.html"
+  },
+  {
+    "revision": "0983c5896382a4a0834c",
+    "url": "/material-dash/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "63b1653d729ec4842fb1",
+    "url": "/material-dash/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "0983c5896382a4a0834c",
+    "url": "/material-dash/static/js/2.a2d20ecf.chunk.js"
+  },
+  {
+    "revision": "286624c8d95769e3759a2a7869634d07",
+    "url": "/material-dash/static/js/2.a2d20ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b1653d729ec4842fb1",
+    "url": "/material-dash/static/js/main.3156e28e.chunk.js"
+  },
+  {
+    "revision": "b7e722c013f806f9e92e",
+    "url": "/material-dash/static/js/runtime-main.3fb0bcef.js"
+  }
+]);
